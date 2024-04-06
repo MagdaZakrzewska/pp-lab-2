3d7ae5cc4f36fcd8c3833f2da9a16906af9de9e1
Magdalena Zakrzewska <magdazakrzewskamagda@gmaill.com>
2024-04-06 16:34:35 +0200

    chore: add zadanie1 file

--- zadanie1.java
+++ zadanie1.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-
 public class zadanie1 {
     public static void main(String[] args){
         System.out.println("Podaj liczbę przedmiotów, z których chcesz obliczyć średnią arytmetyczną: ");
         Scanner wczytaj = new Scanner(System.in);
+        
         int numberOfSubjects = wczytaj.nextInt();
         int[] oceny = new int[numberOfSubjects];
+        
+        float suma = 0;
         for(int i = 0; i < oceny.length; i++){
             System.out.println("Podaj ocenę: ");
             oceny[i] = wczytaj.nextInt();
+            suma += oceny[i];
         }
-        int suma = 0;
-        for (int j = 0; j < oceny.length; j++){
-            suma = suma + oceny[j];
-        }
+        
         float avg = suma/numberOfSubjects;
         System.out.println("Średnia arytmetyczna: " + avg);
         wczytaj.close();
