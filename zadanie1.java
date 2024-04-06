e9f1c3077fceecc7ab8f5a6db83ffe9aa7fce998
Magdalena Zakrzewska <magdazakrzewskamagda@gmaill.com>
2024-04-06 15:38:59 +0200

    chore: add zadanie1 file

--- zadanie1.java
+++ zadanie1.java
@@ -1,5 +1,21 @@
-package pp-lab-2;
+import java.util.Scanner;
 
 public class zadanie1 {
-
+    public static void main(String[] args){
+        System.out.println("Podaj liczbę przedmiotów, z których chcesz obliczyć średnią arytmetyczną: ");
+        Scanner wczytaj = new Scanner(System.in);
+        int numberOfSubjects = wczytaj.nextInt();
+        int[] oceny = new int[numberOfSubjects];
+        for(int i = 0; i < oceny.length; i++){
+            System.out.println("Podaj ocenę: ");
+            oceny[i] = wczytaj.nextInt();
+        }
+        int suma = 0;
+        for (int j = 0; j < oceny.length; j++){
+            suma = suma + oceny[j];
+        }
+        float avg = suma/numberOfSubjects;
+        System.out.println("Średnia arytmetyczna: " + avg);
+        wczytaj.close();
+    }
 }
